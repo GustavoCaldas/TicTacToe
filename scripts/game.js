be9e755f9ec7e3b3a9bfcb0cea014620cce66989
Gustavo Caldas de Souza <gustavocaldes@gmail.com>
2020-12-21 00:31:25 -0300

    script 1.5

--- scripts/game.js
+++ scripts/game.js
@@ -2,43 +2,65 @@ let grid = [null, null, null,
     null, null, null,
     null, null, null];
 
-let jogadasvencedoras = [];
+let winnersPlay = [[0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [0,4,8],                       
+                    [2,4,6]];
 
 let lastPlay = null;
 let currentPlay = null;
 
 function controlPlaytime(object){
+    // Control the time of every player on the game.
     currentPlay = object.target.id[7];
     if (lastPlay != null){
         if(lastPlay == 'x' && currentPlay == 'x'){
-            alert("Esta peca ja foi usada");
+            alert("This piece you have already used!");
+            alert("Choose another one!");
             currentPlay = null;
         }else if (lastPlay == 'o' && currentPlay == 'o'){
-            alert("Esta peca ja foi usada");
+            alert("This piece you have already used!");
+            alert("Choose another one!");
             currentPlay = null;
         }
     }
 }
 
-function playMove(pos, id){
+function playMove(pos, object){
+    // Check if the position that user choose is
+    // blank, if it is we fill the square with a image.
     if(currentPlay != null){
         if(grid[pos] == null){
             grid[pos] = currentPlay;
             lastPlay = currentPlay;
             if(currentPlay == 'x'){
-                id.style.backgroundImage = "url('../assets/icns/X.png')";
+                object.style.backgroundImage = "url('../assets/icns/X.png')";
             }else if(currentPlay == 'o'){
-                id.style.backgroundImage = "url('../assets/icns/O.png')";
+                object.style.backgroundImage = "url('../assets/icns/O.png')";
             }
+            currentPlay = null;
         }else{
-            alert("Este espaco esta preenchido!");
+            alert("This square is filled!");
+            alert("Choose another one!");
         }
-        currentPlay = null;
     }else{
-        alert("Selecione uma peca!");
+        alert("Choose a piece!");
     }
 }
 
 function checkWinner(){
-     
+    // Verify if someone wins the game.
+    winnersPlay.forEach((element) => {
+        if(grid[element[0]] != null || grid[element[1]] != null
+            || grid[element[2]] != null){
+                if(grid[element[0]] === grid[element[1]] &&
+                    grid[element[0]] === grid[element[2]]){
+                        alert("Someone Win!");
+                    }
+            }
+    })
 }
--- scripts/interface.js
+++ scripts/interface.js
@@ -6,15 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
         square.addEventListener('click', handleClick);
     })
 
-    botao_x = document.getElementById("button_x");
-    botao_x.addEventListener('click', controlPlaytime);
+    button_x = document.getElementById("button_x");
+    button_x.addEventListener('click', controlPlaytime);
 
-    botao_o = document.getElementById("button_o");
-    botao_o.addEventListener('click', controlPlaytime);
+    button_o = document.getElementById("button_o");
+    button_o.addEventListener('click', controlPlaytime);
 
     function handleClick(object){
         let pos = object.target.id[7];
         playMove(pos, object.target);
-        checkWinner();
+        setTimeout(checkWinner, 20);
     }
 })
